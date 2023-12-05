54c00c2b701905e8f214f3176c366d1546d7c915
Tharon Nithiroongrujakorn <tharonn@gmail.com>
2023-12-05 22:30:22 +0800

    Update forms with docstrings, formating

--- models.py
+++ models.py
@@ -1,10 +1,8 @@
-from enum import Enum
 from typing import List
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from sqlalchemy import Date, Float, ForeignKey, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy.sql import func
 import datetime as dt
 
 # https://stackoverflow.com/questions/9692962/flask-sqlalchemy-import-context-issue/9695045#9695045 for seperation of model from main
@@ -13,7 +11,11 @@ db = SQLAlchemy()
 
 # Customers
 
+
 class User(UserMixin, db.Model):
+    """Flask login user model, customer details should be null if role is
+    'admin' or 'employee'"""
+
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -24,9 +26,13 @@ class User(UserMixin, db.Model):
     role: Mapped[str] = mapped_column(String, default="user", nullable=False)
 
     # customerDetails nullable if employee/admin
-    customerDetails: Mapped["Customer"] = relationship("Customer")  # type: ignore
+    customerDetails: Mapped["CustomerDetails"] = relationship("Customer")
+
 
 class CustomerDetails(db.Model):
+    """Customer's particulars, also parent of shopping cart, addresses,
+    and order invoices, child of user model"""
+
     __tablename__ = "customers"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -34,11 +40,11 @@ class CustomerDetails(db.Model):
     last_name: Mapped[str] = mapped_column(String, nullable=False)
     date_of_birth: Mapped[dt.datetime] = mapped_column(Date(), nullable=False)
     phone_number: Mapped[str] = mapped_column(String, nullable=False)
-    
     # Parent
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    user: Mapped["User"] = relationship(back_populates="customerDetails", single_parent=True) # single_parent=True enforces one parent 
-
+    user: Mapped["User"] = relationship(
+        back_populates="customerDetails", single_parent=True
+    )  # single_parent=True enforces one parent
     # Children
     # shopping cart 1>many
     shoppingcart: Mapped[List["ShoppingCart"]] = relationship(back_populates="customer")
@@ -47,7 +53,10 @@ class CustomerDetails(db.Model):
     # orders
     orders: Mapped[List["Order"]] = relationship(back_populates="customer")
 
+
 class Address(db.Model):
+    """Customer address, child of customer details"""
+
     __tablename__ = "addresses"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -59,15 +68,24 @@ class Address(db.Model):
     country: Mapped[str] = mapped_column(String)
 
     customer_id: Mapped[int] = mapped_column(ForeignKey("customers.id"))
-    customer: Mapped["CustomerDetails"] = relationship(back_populates="addresses", single_parent=True)
+    customer: Mapped["CustomerDetails"] = relationship(
+        back_populates="addresses", single_parent=True
+    )
+
 
 # Point of sale models
 class ShoppingCart(db.Model):
+    """Customer's shopping cart, child of customer's details, but is parent of one shopping cart
+    Shopping cart of a user is formed from a select query on customer_id, and retrieves it.
+    """
+
     __tablename__ = "shoppingcarts"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     # Server sets added time
-    date_added: Mapped[dt.datetime] = mapped_column(Date(), nullable=False, server_default=func.now())
+    date_added: Mapped[dt.datetime] = mapped_column(
+        Date(), nullable=False, default=dt.datetime.now().date()
+    )
     quantity: Mapped[int] = mapped_column(Integer, nullable=False)
 
     customer_id: Mapped[int] = mapped_column(ForeignKey("customers.id"))
@@ -77,23 +95,32 @@ class ShoppingCart(db.Model):
 
     product: Mapped["Product"] = relationship()
 
+
 class Order(db.Model):
+    """Customer's sales invoice, generated AFTER checkout and store as part of order history reference, child of customer details, parent of list of products"""
+
     __tablename__ = "orders"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
 
-    order_date: Mapped[dt.datetime] = mapped_column(Date(), nullable=False, server_default=func.now())
-    delivery_date: Mapped[dt.datetime] = mapped_column(Date(), nullable=False, server_default=func.now())
-    status: Mapped[str] = mapped_column(Enum('pending', 'delivered'), default='pending')
-    
+    order_date: Mapped[dt.datetime] = mapped_column(
+        Date(), nullable=False, default=dt.datetime.now().date()
+    )
+    delivery_date: Mapped[dt.datetime] = mapped_column(
+        Date(), nullable=False, default=dt.datetime.now().date()
+    )
+    status: Mapped[str] = mapped_column(String, default="pending", nullable=False)
+
     # TODO address, either service layer copy/paste or set as relationship
     customer_id: Mapped[int] = mapped_column(ForeignKey("customers.id"))
     customer: Mapped["CustomerDetails"] = relationship(back_populates="orders")
 
 
 class Product(db.Model):
+    """Product data, has relationship with"""
+
     __tablename__ = "products"
-    
+
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String, nullable=False)
     price: Mapped[float] = mapped_column(Float, nullable=False)
@@ -103,4 +130,6 @@ class Product(db.Model):
     stock: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
 
     cart_id: Mapped[int] = mapped_column(ForeignKey("shoppingcarts.id"))
-    cart: Mapped["ShoppingCart"] = relationship(back_populates="product", single_parent=True)
+    cart: Mapped["ShoppingCart"] = relationship(
+        back_populates="product", single_parent=True
+    )
